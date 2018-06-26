5a9e90fafc0fe39371031ebb5d4ef5de20013250
tom.louis.murphy <tom.louis.murphy@gmail.com>
2018-06-25 23:57:23 -0500

    developed movement functions

--- script001.py
+++ script001.py
@@ -44,3 +44,115 @@ def select_location(x):
 		x.location = 'bottom right'
 
 select_location(player1)
+
+def move_north(x):
+	if player1.location == 'middle left':
+		middle_location_array[0].inhabitants.remove(x)
+		top_location_array[0].inhabitants.append(x)
+		x.location = 'top left'
+	elif player1.location == 'middle middle':
+		middle_location_array[1].inhabitants.remove(x)
+		top_location_array[1].inhabitants.append(x)
+		x.location = 'top middle'
+	elif player1.location == 'middle right':
+		middle_location_array[2].inhabitants.remove(x)
+		top_location_array[2].inhabitants.append(x)
+		x.location = 'top right'
+	elif player1.location == 'bottom left':
+		bottom_location_array[0].inhabitants.remove(x)
+		middle_location_array[0].inhabitants.append(x)
+		x.location = 'middle left'
+	elif player1.location == 'bottom middle':
+		bottom_location_array[1].inhabitants.remove(x)
+		middle_location_array[1].inhabitants.append(x)
+		x.location = 'middle middle'
+	elif player1.location == 'bottom right':
+		bottom_location_array[2].inhabitants.remove(x)
+		middle_location_array[2].inhabitants.append(x)
+		x.location = 'middle right'
+	else
+		pass
+
+def move_east(x):
+	if player1.location == 'top left':
+		top_location_array[0].inhabitants.remove(x)
+		top_location_array[1].inhabitants.append(x)
+		x.location = 'top middle'
+	elif player1.location == 'top middle':
+		top_location_array[1].inhabitants.remove(x)
+		top_location_array[2].inhabitants.append(x)
+		x.location = 'top right'
+	elif player1.location == 'middle left':
+		middle_location_array[0].inhabitants.remove(x)
+		middle_location_array[1].inhabitants.append(x)
+		x.location = 'middle middle'
+	elif player1.location == 'middle middle':
+		middle_location_array[1].inhabitants.remove(x)
+		middle_location_array[2].inhabitants.append(x)
+		x.location = 'middle right'
+	elif player1.location == 'bottom left':
+		bottom_location_array[0].inhabitants.remove(x)
+		bottom_location_array[1].inhabitants.append(x)
+		x.location = 'bottom middle'
+	elif player1.location == 'bottom middle':
+		bottom_location_array[1].inhabitants.remove(x)
+		bottom_location_array[2].inhabitants.append(x)
+		x.location = 'bottom right'
+	else
+		pass
+
+def move_south(x):
+	if player1.location == 'top left':
+		top_location_array[0].inhabitants.remove(x)
+		middle_location_array[0].inhabitants.append(x)
+		x.location = 'middle left'
+	elif player1.location == 'top middle':
+		top_location_array[1].inhabitants.remove(x)
+		middle_location_array[1].inhabitants.append(x)
+		x.location = 'middle middle'
+	elif player1.location == 'top right':
+		top_location_array[2].inhabitants.remove(x)
+		middle_location_array[2].inhabitants.append(x)
+		x.location = 'middle right'
+	elif player1.location == 'middle left':
+		middle_location_array[0].inhabitants.remove(x)
+		bottom_location_array[0].inhabitants.append(x)
+		x.location = 'bottom left'
+	elif player1.location == 'middle middle':
+		middle_location_array[1].inhabitants.remove(x)
+		bottom_location_array[1].inhabitants.append(x)
+		x.location = 'bottom middle'
+	elif player1.location == 'middle right':
+		middle_location_array[2].inhabitants.remove(x)
+		bottom_location_array[2].inhabitants.append(x)
+		x.location = 'bottom right'
+	else
+		pass
+
+def move_west(x):
+	if player1.location == 'top middle':
+		top_location_array[1].inhabitants.remove(x)
+		top_location_array[0].inhabitants.append(x)
+		x.location = 'top left'
+	elif player1.location == 'top right':
+		top_location_array[2].inhabitants.remove(x)
+		top_location_array[1].inhabitants.append(x)
+		x.location = 'top middle'
+	elif player1.location == 'middle middle':
+		middle_location_array[1].inhabitants.remove(x)
+		middle_location_array[0].inhabitants.append(x)
+		x.location = 'middle left'
+	elif player1.location == 'middle right':
+		middle_location_array[2].inhabitants.remove(x)
+		middle_location_array[1].inhabitants.append(x)
+		x.location = 'middle middle'
+	elif player1.location == 'bottom middle':
+		bottom_location_array[1].inhabitants.remove(x)
+		bottom_location_array[0].inhabitants.append(x)
+		x.location = 'bottom left'
+	elif player1.location == 'bottom right':
+		bottom_location_array[2].inhabitants.remove(x)
+		bottom_location_array[1].inhabitants.append(x)
+		x.location = 'bottom middle'
+	else
+		pass
